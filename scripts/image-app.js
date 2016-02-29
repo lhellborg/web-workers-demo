5c57f548e507c3a53508f09f89bb8eaa9fd47a00
Linda Hellborg <linda.hellborg@gmail.com>
2016-02-29 09:44:11 +0100

    fixed manipulation loop and web worker

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -6,6 +6,9 @@
   var canvas = document.querySelector('#image');
   var ctx = canvas.getContext('2d');
 
+  var webWorker = new Worker('scripts/worker.js');
+
+
   function handleImage(e){
     var reader = new FileReader();
     reader.onload = function(event){
@@ -42,36 +45,31 @@
 
     // Hint! This is where you should post messages to the web worker and
     // receive messages from the web worker.
-    var webWorker = new Worker('worker.js');
     calling = function() {
-      webWorker.postMessage(imageData);
+      webWorker.postMessage({
+        "imageData":imageData,
+        "type":type})
       console.log("message to worker");
-    }
+      }
+      calling();
+    }; // end manipulateImage
 
-    calling();
 
    webWorker.onmessage = function(e) {
-    imageData = e.data.imageData;
-    console.log('Message received from worker');
-  }
-
-  webWorker.onmessage();
-
-    // length = imageData.data.length / 4;
-    // for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-    //   r = imageData.data[i * 4 + 0];
-    //   g = imageData.data[i * 4 + 1];
-    //   b = imageData.data[i * 4 + 2];
-    //   a = imageData.data[i * 4 + 3];
-    //   pixel = manipulate(type, r, g, b, a);
-    //   imageData.data[i * 4 + 0] = pixel[0];
-    //   imageData.data[i * 4 + 1] = pixel[1];
-    //   imageData.data[i * 4 + 2] = pixel[2];
-    //   imageData.data[i * 4 + 3] = pixel[3];
-    // }
     toggleButtonsAbledness();
-    return ctx.putImageData(imageData, 0, 0);
+    var image = e.data;
+    if (image) return ctx.putImageData(e.data, 0, 0);
+    console.log('No message received from worker');
   };
+
+
+  // webWorker.onerror = function(error) {
+  //   function WorkerException(message) {
+  //     this.name = "WorkerException";
+  //     this.message = message;
+  //   };
+  //   throw new WorkerException("Worker error.");
+  // };
 
   function revertImage() {
     return ctx.putImageData(original, 0, 0);
